4f86c128a2580e94919027dc38e30b1e5aa00bec
Peter Santyago <159805755+metaversecapitals@users.noreply.github.com>
2024-02-18 19:12:08 +0100

    Update #2

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Request, Response } from "express";
+import request from 'supertest';
 
 const app = express();
 const PORT = 3000;
@@ -10,4 +11,14 @@ app.get('/', (req: Request, res: Response) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running in http://localhost:${PORT}`);
+});
+
+describe('Basic Express App', () => {
+    it('Should respond with "Hello World" for the GET method', () => {
+        return request(app).get('/')
+          .then(response => {
+            expect(response.statusCode).toBe(200);
+            expect(response.text).toBe('Hello World');
+          });
+    });
 });
